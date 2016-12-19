ef0e677afa881a4bc511a6ea5b5ee07d39b2a4a3
Brian Lee <brianjleeofcl@gmail.com>
2016-12-19 11:03:16 -0800

    Fix CORS preflight

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -36,8 +36,10 @@ app.use(morgan(':body'));
 app.use((req, res, next) => {
   if (req.get('Origin') === 'http://experimental.brianjlee.net') {
     res.set({
-      'Access-Control-Allow-Origin': 'http://experimental.brianjlee.net',
-      'Access-Control-Allow-Credentials': 'true'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE'
     });
   }
   next();
