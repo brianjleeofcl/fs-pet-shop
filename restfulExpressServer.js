36bb7b8e121eaf88cec13afc3a73d61c8ee71e93
Brian Lee <brianjleeofcl@gmail.com>
2016-12-19 11:08:13 -0800

    Add OPTIONS allow

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
       'Access-Control-Allow-Origin': 'http://experimental.brianjlee.net',
       'Access-Control-Allow-Credentials': 'true',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE'
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, OPTIONS'
     });
   }
   next();
